f52ffe53ab4b02948ac60d64b73c8796b05c9590
Miguel Lozano <miguel.lozano737@gmail.com>
2019-02-13 17:21:15 -0300

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantsController < ApplicationController
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: "McDonalds", address: "Everywhere", phone_number: "54 11 55767656", category: "chinese", review: 5)
+Restaurant.create(name: "Bellagamba", address: "Cabrera 2324", phone_number: "54 11 55767657", category: "italian", review: 2)
+Restaurant.create(name: "SushiClub", address: "Cabrera 2325", phone_number: "54 11 55767658", category: "japanese", review: 3)
+Restaurant.create(name: "Belgian", address: "Cabrera 2328", phone_number: "54 11 55767660", category: "belgian", review: 1)
+Restaurant.create(name: "Novecento", address: "Cabrera 2329", phone_number: "54 11 55767661", category: "french", review: 4)
+
+puts "Seed finished"
